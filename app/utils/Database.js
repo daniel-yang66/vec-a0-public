b96c162a2c1b298bf01e66cb8b7107040f992826
daniel-yang66 <daniel.yang787@outlook.com>
2026-07-04 11:17:22 -0700

    rid of console.log

--- app/utils/Database.js
+++ app/utils/Database.js
@@ -1,41 +1,5 @@
-// import { tablesDB } from "../lib/supabase";
-// import { Query } from "appwrite";
 import { createClient } from "../lib/supabase";
-// export async function getAirportDetails(origin, destination) {
-//   const data = await tablesDB.listRows({
-//     databaseId: process.env.NEXT_PUBLIC_DATABASE_ID,
-//     tableId: process.env.NEXT_PUBLIC_TABLE_ID,
-//     queries: [Query.equal("icao_code", [origin, destination])],
-//   });
 
-//   const og = data.rows.filter((item, i) => item.icao_code === origin)[0];
-//   const dest = data.rows.filter((item, i) => item.icao_code === destination)[0];
-
-//   return { origin: og, destination: dest };
-// }
-
-// export async function getAirlineLogo(code) {
-//   const data = await tablesDB.listRows({
-//     databaseId: process.env.NEXT_PUBLIC_DATABASE_ID,
-//     tableId: process.env.NEXT_PUBLIC_TABLE_ID_3,
-//     queries: [Query.limit(1), Query.equal("iata", code)],
-//   });
-//   return data["rows"];
-// }
-
-// export async function getRunways(origin, destination) {
-//   const data = await tablesDB.listRows({
-//     databaseId: process.env.NEXT_PUBLIC_DATABASE_ID,
-//     tableId: process.env.NEXT_PUBLIC_TABLE_ID_2,
-//     queries: [Query.equal("airport_ident", [origin, destination])],
-//   });
-//   const ogRwy = data.rows.filter((item, i) => item.airport_ident === origin);
-//   const destRwy = data.rows.filter(
-//     (item, i) => item.airport_ident === destination
-//   );
-
-//   return { origin: ogRwy, destination: destRwy };
-// }
 const supabase = createClient();
 
 export async function GetAirportDetails(origin, destination) {
