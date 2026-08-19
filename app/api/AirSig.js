05b8ef10b8c5026fd59272a24655897abd333ea9
daniel-yang66 <daniel.yang787@outlook.com>
2026-08-18 22:57:29 -0400

    bug fix with awc api calls

--- app/api/AirSig.js
+++ app/api/AirSig.js
@@ -1,27 +1,43 @@
 "use server";
 export default async function AirSig() {
+  let gAir, dSig, air, iSig;
   try {
     const gAirData =
       await fetch(`https://aviationweather.gov/api/data/gairmet?format=json
   `);
-    const gAir = await gAirData.json();
+    try {
+      gAir = await gAirData.json();
+    } catch {
+      gAir = [];
+    }
 
     const dSigData =
-      await fetch(`https://aviationweather.gov/api/data/airsigmet?format=json&type=sigmet
-`);
+      await fetch(`https://aviationweather.gov/api/data/airsigmet?format=json&types=sigmet
+    `);
 
-    const dSig = await dSigData.json();
+    try {
+      dSig = await dSigData.json();
+    } catch {
+      dSig = [];
+    }
 
     const airData =
       await fetch(`https://aviationweather.gov/api/data/airmet?format=json
-  `);
-    const air = await airData.json();
-
+      `);
+    try {
+      air = await airData.json();
+    } catch {
+      air = [];
+    }
     const iSigData =
       await fetch(`https://aviationweather.gov/api/data/isigmet?format=json
-`);
+    `);
 
-    const iSig = await iSigData.json();
+    try {
+      iSig = await iSigData.json();
+    } catch {
+      iSig = [];
+    }
 
     return { iSig: iSig, air: air, gAir: gAir, dSig: dSig };
   } catch {
